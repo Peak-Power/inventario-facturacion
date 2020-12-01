204da02daa76dd84d8932e92dcc901fa476c1b71
Peak Power - Szwedowski <maruydiegorematadores@ggmail.com>
2020-12-01 20:40:32 -0300

    Inventario y Facturación - Szwedowski Bermúdez

---
+++ FACTURAR/config/db.php
@@ -0,0 +1,18 @@
+<?php
+/*Datos de conexion a la base de datos*/
+define('DB_HOST', 'localhost');//DB_HOST:  generalmente suele ser "127.0.0.1"
+define('DB_USER', 'root');//Usuario de tu base de datos
+define('DB_PASS', '');//Contraseña del usuario de la base de datos
+define('DB_NAME', 'simple_stock');//Nombre de la base de datos
+ 
+/*Datos de la empresa*/
+define('NOMBRE_EMPRESA', 'CLEO SEXY CLUB');
+define('DIRECCION_EMPRESA', 'Ing Luis Ponce 1404');
+define('TELEFONO_EMPRESA', '+(598) 99 5511 135');
+define('EMAIL_EMPRESA', 'contacto@cleo-sexy-club.com');
+define('TAX', '22');
+
+
+
+
+?>
